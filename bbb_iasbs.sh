92fef269f95dd2322a322db428bb5664f5b78820
Aydin Mohandesi <aidin.mohandesi@gmail.com>
2020-11-13 14:33:30 +0330

    Fix bugs.

--- bbb_iasbs.sh
+++ bbb_iasbs.sh
@@ -15,14 +15,14 @@ cp ./files/default.pdf /var/www/bigbluebutton-default/
 echo "Change Max Upload File from 30M to 80M"
 export MAX_F=80;
 sed -i "s~maxFileSizeUpload=30000000~maxFileSizeUpload=${MAX_F}000000~g" /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
-sed -i "s/30000000/${MAX_F}000000/g" /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+sed -i "s~30000000~${MAX_F}000000~g" /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
 sed -i "s~30m~${MAX_F}m~g" /etc/bigbluebutton/nginx/web.nginx
 
 echo "Change default error pages"
 rm /var/www/bigbluebutton-default/*.html  
 cp ./files/html/*.html /usr/share/nginx/html/
 cp ./files/html/index.html /var/www/bigbluebutton-default/
-cat "$HOST_NAME" > /var/www/bigbluebutton-default/index.html
+echo $HOST_NAME > /var/www/bigbluebutton-default/index.html
 sed -i "s~elearn~$HOST_NAME~g" /usr/share/nginx/html/*.html
 sed -i "s~#error_page  404  /404.html;~error_page  404  /404.html;\n  location / {\n    root   /var/www/bigbluebutton-default;\n    index  index.html index.htm;\n    expires 1m;\n  }~g" /etc/nginx/sites-available/bigbluebutton
 sed -i "s~500 502 503 504  /50x.html~403 404 500 502 503 504 /x.html~g" /etc/nginx/sites-available/bigbluebutton
