b1d81604b9ee824e77fff731cc4f555e373ddd91
Abbas Sadati <s.a.sadati@iasbs.ac.ir>
2021-01-16 02:43:09 +0330

    chang config to raya

--- apply-config.sh
+++ apply-config.sh
@@ -5,18 +5,13 @@ source /etc/bigbluebutton/bbb-conf/apply-lib.sh
 
 #enableUFWRules
 
-echo "Warning: change external_rtp_ip and external_sip_ip to the public IP of your BBB server."
-
-echo "Running three parallel Kurento media server"
-enableMultipleKurentos
-
 echo "Make the HTML5 client default"
 sed -i 's~attendeesJoinViaHTML5Client=.*~attendeesJoinViaHTML5Client=true~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 sed -i 's~moderatorsJoinViaHTML5Client=.*~moderatorsJoinViaHTML5Client=true~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 
 echo "Set Welcome message"
 sed -i 's~defaultWelcomeMessage=.*~defaultWelcomeMessage=Welcome to <b>%%CONFNAME%%</b>!~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
-sed -i 's~defaultWelcomeMessageFooter=.*~defaultWelcomeMessageFooter=Use a <b>headset</b> to avoid causing background noise.<br>Refresh the browser in case of any network issue.<br>Help: <a target="_blank" href="https://iasbs.ac.ir/iasbs-elearn">IASBS E-Learning</a>~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+sed -i 's~defaultWelcomeMessageFooter=.*~defaultWelcomeMessageFooter=Use a <b>headset</b> to avoid causing background noise.<br>Refresh the browser in case of any network issue.~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 
 echo "Let Moderators unmute users"
 sed -i 's~allowModsToUnmuteUsers=.*~allowModsToUnmuteUsers=true~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
@@ -49,7 +44,7 @@ echo "Enable - See other users in the Users list"
 sed -i 's~lockSettingsHideUserList=.*~lockSettingsHideUserList=false~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 
 echo "Prevent viewers from sharing webcams"
-sed -i 's~lockSettingsDisableCam=.*~lockSettingsDisableCam=true~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
+sed -i 's~lockSettingsDisableCam=.*~lockSettingsDisableCam=false~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
 
 echo "Allow users from joining classes from multiple devices"
 sed -i 's~allowDuplicateExtUserid=.*~allowDuplicateExtUserid=true~g' /usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties
@@ -67,10 +62,10 @@ echo "Audio check"
 sed -i 's~skipCheck:.*~skipCheck: false~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
 
 echo "Set Client Title"
-sed -i 's~clientTitle:.*~clientTitle: IASBS~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+sed -i 's~clientTitle:.*~clientTitle: Raya~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
 
 echo "Set App Title"
-sed -i 's~appName:.*~appName: IASBS E-Learning~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+sed -i 's~appName:.*~appName: Raya~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
 
-echo "Set Helplink"
-sed -i 's~helpLink:.*~helpLink: http://iasbs.ac.ir/iasbs-elearn~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
+#echo "Set Helplink"
+#sed -i 's~helpLink:.*~helpLink: http://~g' /usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml
